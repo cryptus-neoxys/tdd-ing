4254166fed1446ab2ab9c320ce647af7159c05f8
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-08-02 23:06:37 +0530

    ✅ 🧪

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,15 +2,32 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const spanish = "Spanish"
+const french = "French"
 
-func Hello(name string) string {
-	if(name == "") {
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name, lang string) string {
+	if name == "" {
 		name = "world"
 	}
-	return englishHelloPrefix + name
+
+	prefix := englishHelloPrefix
+
+	switch lang {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Dev"))
+	fmt.Println(Hello("Dev", "English"))
 }
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -14,15 +14,27 @@ func TestHello(t *testing.T) {
 
 	t.Run("saying hello to people", func(t *testing.T) {
 		want := "Hello, Dev"
-		got := Hello("Dev")
+		got := Hello("Dev", "English")
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run(`Say "Hello, world" when empty string is passed`, func(t *testing.T) {
 		want := "Hello, world"
-		got := Hello("")
+		got := Hello("", "English")
 
 		assertCorrectMessage(t, got, want)
 	})
+	
+    t.Run("in Spanish", func(t *testing.T) {
+        got := Hello("Elodie", spanish)
+        want := "Hola, Elodie"
+        assertCorrectMessage(t, got, want)
+    })
+	
+    t.Run("in French", func(t *testing.T) {
+        got := Hello("Mademoiselle", french)
+        want := "Bonjour, Mademoiselle"
+        assertCorrectMessage(t, got, want)
+    })
 }
