aa5e299d6b6528610a0c2c19c50b2d434c6c2617
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-08-02 22:56:45 +0530

    🧪 (hello_test.go)

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,26 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	want := "Hello, Dev"
-	got := Hello("Dev")
-
-	if want != got {
-		t.Errorf("got %q, want %q", got, want)
+	
+	assertCorrectMessage := func (t testing.TB, got, want string) {
+		t.Helper()
+		
+		if want != got {
+			t.Errorf("got %q, want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		want := "Hello, Dev"
+		got := Hello("Dev")
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run(`Say "Hello, world" when empty string is passed`, func(t *testing.T) {
+		want := "Hello, world"
+		got := Hello("")
+
+		assertCorrectMessage(t, got, want)
+	})
 }
