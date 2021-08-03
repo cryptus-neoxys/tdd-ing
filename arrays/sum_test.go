283baa51d4cef04d963fd3da9939617673b23d32
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-08-03 17:30:18 +0530

    🧪 (sum_test.go)

---
+++ arrays/sum_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+
+	arr := [5]int{1, 2, 3, 4, 5}
+
+	got := Sum(arr)
+	want := 15
+
+	if got != want {
+		t.Errorf("got %d want %d given, %v", got, want, arr)
+	}
+}
