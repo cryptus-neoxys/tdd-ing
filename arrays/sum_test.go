c84ce6085e207ac5330b0508d1c453695d43302c
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-08-10 02:39:27 +0530

    🧪 (sum_test.go)

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,15 +1,31 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
-	arr := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		arr := [5]int{1, 2, 3, 4, 5}
+	
+		got := Sum(arr)
+		want := 15
+	
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, arr)
+		}
+	})
 
-	got := Sum(arr)
-	want := 15
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int {1, 2, 3}
 
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, arr)
-	}
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
 }
