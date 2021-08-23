021a177295569822aadef799491b4e47d3985d09
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-08-23 22:34:04 +0530

    ✅ (shapes*.go) etend tests

--- structs/shapes.go
+++ structs/shapes.go
@@ -23,6 +23,15 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.radius * c.radius
 }
 
+type Triangle struct {
+	base float64
+	height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.base * t.height
+}
+
 func Perimeter(rect Rectangle) float64 {
 	return 2 * (rect.height + rect.width)
 }
