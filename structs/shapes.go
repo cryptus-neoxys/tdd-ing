dcc150b7fc1a0b08203559c57275ecebee06015f
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-08-23 22:24:37 +0530

    ✨ feat: ✅(shape*.go) interfaces

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,8 +1,26 @@
 package main
 
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
-	width float64
+	width  float64
 	height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
 }
 
 func Perimeter(rect Rectangle) float64 {
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestPerimeter (t *testing.T) {
+func TestPerimeter(t *testing.T) {
 	rect := Rectangle{10.0, 10.0}
 	got := Perimeter(rect)
 	want := 40.0
@@ -13,11 +13,23 @@ func TestPerimeter (t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rect := Rectangle{12.0, 6.0}
-	got := Area(rect)
-	want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f, want %.2f", got, want)
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+
+		if got != want {
+			t.Errorf("got %g, want %g", got, want)
+		}
 	}
+
+	t.Run("rectangle", func(t *testing.T) {
+		rect := Rectangle{12.0, 6.0}
+		checkArea(t, rect, 72.0)
+	})
+
+	t.Run("circle", func(t *testing.T) {
+		cir := Circle{10.0}
+		checkArea(t, cir, 314.1592653589793)
+	})
 }
